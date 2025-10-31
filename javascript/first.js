537beb7b1c6aeb28e8143ecc5aefdc3fd488bda9
Fawas-Anayat <fawasinayat.ips@gmail.com>
2025-10-31 19:07:31 +0500

    JS basics

--- javascript/first.js
+++ javascript/first.js
@@ -1,5 +1,5 @@
 /*console.log("hello world")
-console.log("this is the first program of the javascript")*/
+console.log("this is the first program of the javascript")
 
 // the variables in the js are following
 
@@ -8,6 +8,37 @@ const a="gpgc"          //can't be rewritten  later
 var accountEmail="abc@gmail.com"        //this is the old keyword to decalare the variales but it caused the issue of the scope as it is not used as much these days in modern js
 console.log(x,a,accountEmail)
 city="mansehra"  //this is not a recommended way but we can use it
+let school;          //will be treated as the undefined 
+console.log(school);
 
 console.table([x,a,accountEmail,city])
 console.table(typeof[x,a,accountEmail,city])
+
+
+
+
+
+function myfunc(val){
+    return val*=19
+}
+console.log(myfunc(10))
+
+
+const add=(a,b)=>a*b;
+console.log(add(10,3))
+
+let x=1e2
+console.log(x)
+
+let a=10
+let b=20
+
+if(a==b)
+    console.log(true)
+else
+    console.log(false)*/
+
+
+let a=false
+let b= a &&= 10
+console.log(b)
