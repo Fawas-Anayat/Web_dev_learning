b6c71bc9abbd708cd7b2f5f7db99f3a381b8463b
Fawas-Anayat <fawasinayat.ips@gmail.com>
2025-10-31 18:01:53 +0500

    starting the JS

--- javascript/first.js
+++ javascript/first.js
@@ -1,2 +1,13 @@
-console.log("hello world")
-console.log("this is the first program of the javascript")
+/*console.log("hello world")
+console.log("this is the first program of the javascript")*/
+
+// the variables in the js are following
+
+let x=10            // can be rewritten later
+const a="gpgc"          //can't be rewritten  later 
+var accountEmail="abc@gmail.com"        //this is the old keyword to decalare the variales but it caused the issue of the scope as it is not used as much these days in modern js
+console.log(x,a,accountEmail)
+city="mansehra"  //this is not a recommended way but we can use it
+
+console.table([x,a,accountEmail,city])
+console.table(typeof[x,a,accountEmail,city])
