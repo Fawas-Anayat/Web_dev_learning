523a3b956bf3a9ed16cfdea410a2813c7126ef88
Fawas-Anayat <fawasinayat.ips@gmail.com>
2025-11-22 10:10:18 +0500

    js

--- javascript/second.js
+++ javascript/second.js
@@ -1,3 +1,24 @@
-let x=20;
+/*let x=20;
 let y=30;
 console.log(x,y) 
+
+console.log(5=="5")    //true(loose comparison)
+console.log(5==="5")   //false(tight comparison)
+
+*
+
+for(let num of [2,3,4,5,]){  //to traverse the arrays
+    console.log(num)
+}
+
+
+
+function add(a,b){
+    c=a+b;
+    return c
+}
+term=add(5,5)
+console.log(term)*/
+
+
+const add(a,b)=> (a+b)
